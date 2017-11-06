29c1b2b74a92e633dc58980ee2e8d69a3f20cbc8
Tira <217bssr@gmail.com>
2017-11-06 22:38:59 +0000

    05_実習の実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  mount_uploader :thumbnail, UserThumbnailUploader       
-         
+  mount_uploader :thumbnail, UserThumbnailUploader
+  
+  validates :name, presence: true
+  validates :agreement, presence: true, acceptance: {accept: true}
 end
